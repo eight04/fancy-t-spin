61a8bc1d440592910a8b35d440744e9ee4dddba7
eight04 <eight04@gmail.com>
2022-02-24 04:54:12 +0800

    WIP: use img tag with loading=lazy

---
+++ test.mjs
@@ -0,0 +1,8 @@
+import {readFile, writeFile} from "fs";
+
+let content = await readFile("README.md", "utf8");
+content = content.replace(/\[!\[(.*?)]\((.*?)\)]\((.*?)\)/g, (match, alt, img, url) => {
+  return `[<img src="${img}" alt="${alt}" loading="lazy"/>](${url})`;
+});
+await writeFile("README.md", content);
+
